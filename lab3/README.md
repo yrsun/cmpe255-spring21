9c7ee1a04831f1577f52e2bbdaadd6ed9a2a3f36
Yiru Sun <yiru.sun@sjsu.edu>
2021-03-18 15:45:04 -0700

    finished

--- lab3/README.md
+++ lab3/README.md
@@ -2,6 +2,14 @@
 
 [Pima Indian Diabetes dataset](https://www.kaggle.com/uciml/pima-indians-diabetes-database?select=diabetes.csv) from the UCI Machine Learning Repository
 
+## My Output
+
+Experiement | Accuracy | Confusion Matrix | Comment
+Baseline | 0.6770833333333334 | [[114  16] [46 16]] | features: pregnant, insulin, bmi, age
+Solution1 | 0.7708333333333334 | [[115  15] [29 33]] | features: glucose, insulin, bmi, age
+Solution2 | 0.7864583333333334 | [[117  13] [28 34]] | features: pregnant, glucose, insulin, bmi, age
+Solution3 | 0.796875 | [[118  12] [27 35]] | features: pregnant, glucose, bp, insulin, bmi, age
+
 ## Question
 
 * Can we predict the diabetes status of a patient given their health measurements? Build a classifer and calculate Confusion matrix with
--- lab3/lab3.py
+++ lab3/lab3.py
@@ -9,7 +9,6 @@ class DiabetesClassifier:
     def __init__(self) -> None:
         col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
         self.pima = pd.read_csv('diabetes.csv', header=0, names=col_names, usecols=col_names)
-        print(self.pima.head())
         self.X_test = None
         self.y_test = None
         
@@ -19,18 +18,43 @@ class DiabetesClassifier:
         X = self.pima[feature_cols]
         y = self.pima.label
         return X, y
+
+    def define_feature1(self):
+        feature_cols = ['glucose', 'insulin', 'bmi', 'age']
+        X = self.pima[feature_cols]
+        y = self.pima.label
+        return X, y
+
+    def define_feature2(self):
+        feature_cols = ['pregnant', 'glucose', 'insulin', 'bmi', 'age']
+        X = self.pima[feature_cols]
+        y = self.pima.label
+        return X, y
+
+    def define_feature3(self):
+        feature_cols = ['pregnant', 'glucose', 'bp', 'insulin', 'bmi', 'age']
+        X = self.pima[feature_cols]
+        y = self.pima.label
+        return X, y
     
-    def train(self):
+    def train(self, num):
         # split X and y into training and testing sets
-        X, y = self.define_feature()
+        if num == 0:
+            X, y = self.define_feature()
+        elif num == 1:
+            X, y = self.define_feature1()
+        elif num == 2:
+            X, y = self.define_feature2()
+        elif num == 3:
+            X, y = self.define_feature3()
         X_train, self.X_test, y_train, self.y_test = train_test_split(X, y, random_state=0)
         # train a logistic regression model on the training set
         logreg = LogisticRegression()
         logreg.fit(X_train, y_train)
         return logreg
     
-    def predict(self):
-        model = self.train()
+    def predict(self, num):
+        model = self.train(num)
         y_pred_class = model.predict(self.X_test)
         return y_pred_class
 
@@ -50,10 +74,39 @@ class DiabetesClassifier:
         return metrics.confusion_matrix(self.y_test, result)
     
 if __name__ == "__main__":
+    print("Experiement | Accuracy | Confusion Matrix | Comment")
+    rst0 = "Baseline | "
     classifer = DiabetesClassifier()
-    result = classifer.predict()
-    print(f"Predicition={result}")
+    result = classifer.predict(0)
     score = classifer.calculate_accuracy(result)
-    print(f"score={score}")
+    rst0 += f"{score} | "
     con_matrix = classifer.confusion_matrix(result)
-    print(f"confusion_matrix=${con_matrix}")
+    rst0 += f"[{con_matrix[0]} {con_matrix[1]}] | features: pregnant, insulin, bmi, age"
+    print(rst0)
+
+    rst1 = "Solution1 | "
+    classifer = DiabetesClassifier()
+    result = classifer.predict(1)
+    score = classifer.calculate_accuracy(result)
+    rst1 += f"{score} | "
+    con_matrix = classifer.confusion_matrix(result)
+    rst1 += f"[{con_matrix[0]} {con_matrix[1]}] | features: glucose, insulin, bmi, age"
+    print(rst1)
+
+    rst2 = "Solution2 | "
+    classifer = DiabetesClassifier()
+    result = classifer.predict(2)
+    score = classifer.calculate_accuracy(result)
+    rst2 += f"{score} | "
+    con_matrix = classifer.confusion_matrix(result)
+    rst2 += f"[{con_matrix[0]} {con_matrix[1]}] | features: pregnant, glucose, insulin, bmi, age"
+    print(rst2)
+
+    rst3 = "Solution3 | "
+    classifer = DiabetesClassifier()
+    result = classifer.predict(3)
+    score = classifer.calculate_accuracy(result)
+    rst3 += f"{score} | "
+    con_matrix = classifer.confusion_matrix(result)
+    rst3 += f"[{con_matrix[0]} {con_matrix[1]}] | features: pregnant, glucose, bp, insulin, bmi, age"
+    print(rst3)
